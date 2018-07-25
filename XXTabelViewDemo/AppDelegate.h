cebee527831439eefce4b419976213b45e3e77a7
huangwen <huangwen01@corp.netease.com>
2018-07-25 11:36:07 +0800

    Initial Commit

---
+++ XXTabelViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XXTabelViewDemo
+//
+//  Created by awen on 2018/7/25.
+//  Copyright © 2018年 awen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XXTabelViewDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XXTabelViewDemo
+//
+//  Created by awen on 2018/7/25.
+//  Copyright © 2018年 awen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
